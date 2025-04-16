b4549d6e76112fe62362f59fdff4ec3976f80785
vanshchauhan21 <143607775+vanshchauhan21@users.noreply.github.com>
2025-04-16 19:42:26 +0530

    add pika ui

--- components/header.jsx
+++ components/header.jsx
@@ -38,6 +38,18 @@ export default async function Header() {
         {/* Action Buttons */}
         <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
+          <Link href="https://pika-ui-builder.vercel.app/">
+              <Button
+                variant="outline"
+                className="hidden md:inline-flex items-center gap-2"
+              >
+                <LayoutDashboard className="h-4 w-4" />
+          Pika UI
+              </Button>
+              <Button variant="ghost" className="md:hidden w-10 h-10 p-0">
+                <LayoutDashboard className="h-4 w-4" />
+              </Button>
+            </Link>
           <Link href="https://github-profile-readme-generator-iota.vercel.app/">
               <Button
                 variant="outline"
